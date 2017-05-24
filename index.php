c25ce44083f44a0f4fc998892920ffd233288ebe
Piraato <lp.lennartpikijn@gmail.com>
2017-05-24 11:55:22 +0200

    loop

--- index.php
+++ index.php
@@ -12,5 +12,18 @@ if($rob == 'oeps, fo niet ingeleverd'){
 echo 'shit<br>';
 }
 
-echo "hello world my name is Alie";
+echo "hello world my name is Alie<br><br>";
+
+$files = glob("./" ."*");
+
+foreach ($files as $link) {
+
+    $heh = explode('.', $link);
+
+
+    $friendlylink = preg_replace('/[^A-Za-z0-9\-]/', '', $heh[1]);
+
+    echo "<a href='$link'>-> $friendlylink</a><br>";
+}
+
 ?>
