484338d028ee793d277d0d1697d71b3de996f4ed
NiekL <niekluttikhof8@gmail.com>
2017-05-31 13:38:05 +0200

    Contact formulier begin

--- contact.php
+++ contact.php
@@ -14,29 +14,42 @@ include 'hidden.menu.php';
 
 
 <div class="jumbotron">
+
     <div class="container text-center">
         <h2>Contact</h2>
-        <form  id="contactFormulier" action="contact.php" method="POST">
 
 
-                <label class="contactLabel" for="fname">Voornaam</label>
-                <input class="inputForm" type="text" id="fname" name="firstname" placeholder="Uw voornaam..">
+        <form id="contactFormulier" action="#" method="POST">
 
 
+            <label class="contactLabel" for="fname">Voornaam</label>
+            <input class="inputForm" type="text" id="fname" name="firstname" placeholder="Uw voornaam..">
 
-                <label class="contactLabel" for="lname">Achternaam</label>
-                <input class="inputForm" type="text" id="lname" name="lastname" placeholder="Uw achternaam..">
+
+            <label class="contactLabel" for="lname">Achternaam</label>
+            <input class="inputForm" type="text" id="lname" name="lastname" placeholder="Uw achternaam..">
 
             <label class="contactLabel" for="email">E-mail adres</label>
             <input class="inputForm" type="text" id="email" name="email" placeholder="Uw e-mail adres..">
 
 
             <label class="contactLabel" for="subject">Bericht</label>
-            <textarea class="inputForm" id="subject" name="subject" placeholder="Typ hier..." style="height:200px"></textarea>
+            <textarea class="inputForm" id="subject" name="subject" placeholder="Typ hier..."
+                      style="height:200px"></textarea>
 
-            <input id="submitContact" type="submit" value="Submit">
+            <input id="submitContact" type="submit" name="submit" value="Verzenden">
 
         </form>
+
+        <?php
+
+        if (isset($_POST["submit"])) {
+            echo "hoi";
+        }
+
+        ?>
+
+
     </div>
 </div>
 
