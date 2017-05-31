f5274455cf3b940bbd602436a66344f214e6dd30
Piraato <lp.lennartpikijn@gmail.com>
2017-05-31 10:59:29 +0200

    zomerzaandam update

--- test.php
+++ test.php
@@ -1,5 +1,12 @@
 <a href="index.php">Index</a><br><br>
 
+<form action="test.php" method="POST">
+    <input type="text" name="plaats" ><br>
+    <input type="text" name="naam1"><br>
+    <input type="text" name="naam2"><br>
+    <input type="submit" name="klik">
+</form>
+
 <?php
 
 echo "Hello world!";
