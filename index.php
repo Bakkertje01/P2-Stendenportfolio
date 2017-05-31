c454ee0dd812169fad299f5d67ea677fbc81a5f5
Bakkertje01 <ernst-janbakker@live.nl>
2017-05-31 10:26:16 +0200

    Hello world

--- index.php
+++ index.php
@@ -6,24 +6,8 @@
  * Time: 11:34
  */
 
-$rob = 'oeps, fo niet ingeleverd';
 
-if($rob == 'oeps, fo niet ingeleverd'){
-echo 'shit<br>';
-}
-
-echo "hello world my name is Alie<br><br>";
-
-$files = glob("./" ."*");
-
-foreach ($files as $link) {
-
-    $heh = explode('.', $link);
-
-
-    $friendlylink = preg_replace('/[^A-Za-z0-9\-]/', '', $heh[1]);
-
-    echo "<a href='$link'>-> $friendlylink</a><br>";
-}
 
 ?>
+
+test
--- test.php
+++ test.php
@@ -1,27 +1,6 @@
 <a href="index.php">Index</a><br><br>
 
 <?php
-/**
- * Created by PhpStorm.
- * User: superMcNipple
- * Date: 24/05/2017
- * Time: 11:56 -
- */
 
-
-
-function zomerInZaandam($plaats, $naam1, $naam2){
-    echo "Zooooomer in $plaats! $naam1, het is zooomer in $plaats! Ik ga met $naam2 naar $naam1 toeeehhh.";
-}
-
-function rob($aanwezigheid){
-   echo "Hij is op dit moment $aanwezigheid aanwezig. In lokaal 1.060 is een mogelijkheid tot condoleren..";
-}
-
-
-rob('niet');
-
-echo '<br>';
-zomerInZaandam('heerhugowaard', 'Tiri', 'Allah');
 
 ?>
