05de6ac4280f25fa0f9bea26f7d7e041a0d19804
Piraato <lp.lennartpikijn@gmail.com>
2017-05-31 11:36:33 +0200

    Temp. foot & head

--- hidden.menu.php
+++ hidden.menu.php
@@ -20,7 +20,7 @@ include "hidden.style.php";
 
             //namen nog aanpassen uiteraard...
 
-            $files = glob("./" ."*");
+            $files = glob("./" . "*");
 
             echo '<ul class="nav navbar-nav">';
 
@@ -28,13 +28,11 @@ include "hidden.style.php";
 
                 $heh = explode('.', $link);
 
-                if(!empty($heh[2])) {
+                if (!empty($heh[2])) {
                     if ($heh[2] == 'php' || $heh[2] == 'html') {
                         $friendlylink = preg_replace('/[^A-Za-z0-9\-]/', '', $heh[1]);
 
                         echo "<li><a href='$link'>" . ucfirst($friendlylink) . "</a></li>";
-                    } else {
-                        echo '';
                     }
                 }
             }
