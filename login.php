8362cbdeb98239fc76ce7f32eb58c07565cf0800
sanderpaping <sanderpaping@live.nl>
2017-05-31 11:36:25 +0200

    Login pagina

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by Sander Paping.
+ * User: Sander Paping
+ * Date: 31-5-2017
+ * Time: 11:19
+ */
+
+?>
+
+<div id=''>
+
+    <h1>Login</h1>
+
+    <p>Als u gebruik wil maken van deze dienst moet u eerst inloggen. Als dit de eerste keer is dat u <br>
+        deze site bezoekt moet u zich eerst registreren. Na het registreren kunt u inloggen en gebruik <br>
+        maken van onze dienst.<br></p>
+    <form id='login' action='login.php' method='post'>
+        <label for='Gebruikersnaam'>Gebruikersnaam*:</label><br/>
+        <input type='text' name='un' id='Gebruikersnaam' maxlength="50" value=""/><br/>
+
+        <label for='password'>Wachtwoord*:</label><br/>
+        <input type='password' name='pw' id='password' maxlength="50"/><br/>
+        <br/>
+        <table>
+            <tr>
+                <td>
+                    <input id="submit" type='submit' name='submit' value='Aanmelden'/>
+                </td>
+
+    </form>
+</div>
