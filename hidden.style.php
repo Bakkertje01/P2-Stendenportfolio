8be16f9aa8ce81d45e4b1e2c97af17b964fe874b
Piraato <lp.lennartpikijn@gmail.com>
2017-05-31 16:35:50 +0200

    Someone fucked up my code.

--- hidden.style.php
+++ hidden.style.php
@@ -3,7 +3,7 @@
 <head>
     <title>Stenden portfolio</title>
     <meta charset="utf-8">
-    <link rel="icon" type="image/png" href="fav.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="fav.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
