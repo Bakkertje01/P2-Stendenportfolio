24659b89d9ab68865a5c828ccef28684aade2808
Piraato <lp.lennartpikijn@gmail.com>
2017-05-31 15:02:32 +0200

    updates

--- hidden.footer.php
+++ hidden.footer.php
@@ -1,3 +1,3 @@
 <?php
-echo "<center><img src='foot.png' alt='footer' width='100%' height='100' title='Home'></center>";
+echo "<img  style='vertical-align: baseline;' src='foot.png' alt='footer' width='100%' height='100' title='Foot'>";
 ?>
