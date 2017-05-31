d9b4699081f6341bd14c5af2278e22d7ac7d04aa
Piraato <lp.lennartpikijn@gmail.com>
2017-05-31 11:05:58 +0200

    eerste voorbeeld template

---
+++ hidden.template.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+<?php
+
+include 'header.php';
+
+?>
+
+
+<div class="maincontent">
+
+
+    <p>
+        Ayoo funky dudes! Dit is een voorbeeld tamplate, later moet natuurlijk de bootstrap hiern nog in...
+
+    </p>
+
+</div>
+
+
+<?php
+
+include 'footer.php';
+
+?>
+
+</body>
+
+</html>
