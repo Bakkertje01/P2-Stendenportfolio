12c241794132e13c49cd1b35390af144d17cbef8
sanderpaping <sanderpaping@live.nl>
2017-06-06 13:09:26 +0200

    profiel student aangepast

--- hidden.style.php
+++ hidden.style.php
@@ -68,6 +68,33 @@
             border-radius: 4px;
             cursor: pointer;
         }
+        .profile{
+            float: left;
+            margin-top: 5px;
+            margin-right: 10%;
+            margin-left: 10%;
+            margin-bottom: 5px;
+            width: 80%;
+            background-color: aqua;
+
+        }
+
+        #profile-pic{
+            border-radius: 5px;
+            border: 5px;
+            border-color: #000000;
+            float: left;
+            margin:;
+            width: 250px;
+            height: 250px;
+            background-color: #009e13;
+        }
+        #profileinfo{
+            float: left;
+            width: 250px;
+            height: 250px;
+            background-color: yellow;
+        }
 
         #bekijkup {
             float: inherit;
