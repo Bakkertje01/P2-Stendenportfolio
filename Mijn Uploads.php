97c2ae49e27ed1f2f8a409b5490ad47da9908038
Piraato <lp.lennartpikijn@gmail.com>
2017-06-06 11:36:33 +0200

    Verbetrde uploads bekijken

---
+++ Mijn Uploads.php
@@ -0,0 +1,193 @@
+<?php
+
+if (isset($_POST['bekijk'])) {
+
+    $datumin = $_POST['date'];
+
+    if (empty($_POST['map'])) {
+        $map = NULL;
+    } else {
+        $map = $_POST['map'];
+    }
+}
+
+function dateSelect($datum, $folder)
+{
+
+    $dirnamez = "./studentuploads/" . $datum;
+
+    $files1 = glob("$dirnamez/" . "$folder/" . "*");
+
+    if (!empty($folder)) {
+        foreach (array_reverse($files1) as $file1) {
+            $numberOfFiles = count($file1);
+        }
+
+        if ($numberOfFiles > 0) {
+
+            foreach (array_reverse($files1) as $file1) {
+                $filetypes = explode('.', $file1);
+                $fileTitle = explode('(+)', $file1);
+                $filePoster = explode('{-}', $file1);
+
+
+                if ($filetypes[2] == 'jpg' || $filetypes[2] == 'png' || $filetypes[2] == 'jpeg' || $filetypes[2] == 'JPG' || $filetypes[2] == 'gif') {
+                    if (!empty($fileTitle[1])) {
+                        $deftitle = ucfirst($fileTitle[1]);
+
+                        $numberOfFiles = count($files1);
+
+
+                        echo " <div class='col-sm-3'><h5>$deftitle</h5>";
+
+                        echo "<img src='$file1' class='img-responsive' style='width:100%' alt='$file1'>";
+
+                        echo "</div>";
+
+                    }
+
+
+                }
+                if ($filetypes[2] == 'doc' || $filetypes[2] == 'docx' || $filetypes[2] == 'pdf' || $filetypes[2] == 'xls' || $filetypes[2] == 'xlsx') {
+
+                    $deftitle = ucfirst($fileTitle[1]);
+
+
+                    if ($filetypes[2] == 'doc' || $filetypes[2] == 'docx' || $filetypes[2] == 'xls' || $filetypes[2] == 'xlsx') {
+                        $thumbnail = 'woex.jpg';
+                    }
+                    if ($filetypes[2] == 'pdf') {
+                        $thumbnail = 'pdf.jpg';
+                    }
+
+
+                    echo " <div class='col-sm-3'><h5>$deftitle</h5>";
+
+                    echo "<a href='$file1' download><img src='thumbnails/$thumbnail' class='img-responsive'                         style=\"width:100%\" alt=\"$file1\"></a>";
+
+                    echo "</div>";
+                }
+
+
+            }
+        } else {
+            echo "Deze map bevat geen bestanden";
+        }
+
+    }
+
+
+}
+
+if (isset($_POST['reset'])) {
+
+    $datumin = NULL;
+    $mapin = NULL;
+
+}
+
+
+$naam = "Uit session";
+$studentquote = "Quote Uit Session";
+
+
+?>
+
+
+<html>
+
+<head>
+    <link rel="icon" type="image/png" href="fav.png"/>
+</head>
+
+<body>
+<?php
+include 'hidden.header.php';
+include 'hidden.menu.php';
+?>
+
+
+<div class="jumbotron">
+    <div class="container text-center">
+        <h3>Bestanden van <?php echo $naam; ?></h3>
+        <p><i>'<?php echo $studentquote; ?>'</i></p>
+    </div>
+</div>
+
+<div class="container text-center">
+
+
+    <form action="Mijn%20Uploads.php" method="post" enctype="multipart/form-data">
+
+
+        <h3>Selecteer map:</h3>
+
+        <?php
+
+        $dropoptions = array('CV', 'Afbeeldingen', 'Documenten');
+
+        echo "<select name='date'>";
+        foreach ($dropoptions as $item) {
+
+
+            $finalnameoffolder = str_replace($exlude, '', $item);
+
+
+            echo "<option value='$finalnameoffolder'>$finalnameoffolder</option>";
+        }
+        echo "</select>";
+
+        ?>
+
+
+        <p>
+        <center><input id="bekijkup" type="submit" name="bekijk"
+                       value="Verstuur">
+            <input id="bekijkup" type="submit" name="reset" value="Reset"></center>
+        </p>
+
+
+    </form>
+
+    <div>
+
+        <p><?php
+
+            if (isset($_POST['bekijk'])) {
+
+                $studentnumber = 'henk/'; //DIT MOET UIT DE SESSION KOMEN!!!
+
+                $finalnameoffolder = $studentnumber;
+
+                $Typefolder = $_POST['date'];
+
+                $testTheFolder = $finalnameoffolder . $Typefolder;
+
+                echo dateSelect($studentnumber, $Typefolder);
+
+            }
+
+            ?></p>
+    </div>
+
+
+</div>
+
+<?php
+
+include 'hidden.footer.php';
+?>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
