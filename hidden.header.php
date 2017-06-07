60d3319f60f2af6f292fdee726d1cc7c16ce7ed1
NiekL <niekluttikhof8@gmail.com>
2017-06-07 12:54:34 +0200

    'Header aangepast'

--- hidden.header.php
+++ hidden.header.php
@@ -1,4 +1,46 @@
 <link rel="icon" type="image/png" href="fav.png"/>
-<?php
-echo "<center><a href='index.php'><img src='header.png' alt='header' width='100%' height='100' title='Home'></a></center>";
-?>
+<style>
+    #headerTest {
+        height: 100px;
+        background: #0431B4; /* For browsers that do not support gradients */
+        background: linear-gradient(#156caf, #003f70); /* Standard syntax (must be last) */
+    }
+
+    #headerContent{
+        margin-left:35%;
+    }
+
+    #Logo {
+        margin-top: 15px;
+        float:left;
+    }
+
+    #tekst{
+        margin-top: 15px;
+        float:left;
+        color:white;
+        font-family: Lucida Console;
+        font-size: 60px;
+        margin-left:20px;
+    }
+
+</style>
+
+<div id="headerTest">
+
+    <div id="headerContent">
+
+        <div id="Logo">
+            <a href='index.php'><img src='logoStenden.png' alt='header' title='Home'></a>
+
+
+        </div>
+
+        <div id="tekst">
+            Portfolio
+        </div>
+
+    </div>
+
+
+</div>
