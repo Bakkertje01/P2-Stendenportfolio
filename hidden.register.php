919d475a90e7b3f284366169a7bda1de1942ff4a
Bakkertje01 <ernst-janbakker@live.nl>
2017-06-13 19:22:28 +0200

    register aanpassingen

--- hidden.register.php
+++ hidden.register.php
@@ -6,12 +6,13 @@
 
 <body>
 
-<?php include 'include/db_connection.php';?>
-<?php include 'include/functions.php';?>
-<?php include 'include/session.php';?>
+<?php include 'include/db_connection.php'; ?>
+<?php include 'include/functions.php'; ?>
+<?php include 'include/session.php'; ?>
 <?php
 include 'hidden.header.php';
-ob_start(); include 'hidden.menu.php';
+ob_start();
+include 'hidden.menu.php';
 
 ?>
 
@@ -25,80 +26,120 @@ ob_start(); include 'hidden.menu.php';
                     <h1> Registratie </h1>
 
                     <form id='register' action='hidden.register.php' method='post'>
-                        <label for='firstname' >Voornaam*: </label><br/>
-                        <input type='text' name='Voornaam' id='Voornaam' maxlength="30" /><br/>
+                        <label for='firstname'>Voornaam*: </label><br/>
+                        <input type='text' name='Voornaam' id='Voornaam' maxlength="30"/><br/>
 
-                        <label for='lastname' >Achternaam*: </label><br/>
-                        <input type='text' name='Achternaam' id='Achternaam' maxlength="30" /><br/>
+                        <label for='lastname'>Achternaam*: </label><br/>
+                        <input type='text' name='Achternaam' id='Achternaam' maxlength="30"/><br/>
 
-                        <label for='email' >Email Address*:</label><br/>
-                        <input type='text' name='Email' id='Email' maxlength="50" /><br/>
+                        <label for='email'>Email Address*:</label><br/>
+                        <input type='text' name='Email' id='Email' maxlength="50"/><br/>
 
-                        <label for='password' >Password*:</label><br/>
-                        <input type='password' name='Wachtwoord' id='Wachtwoord' maxlength="50" /><br/>
+                        <label for='password'>Password*:</label><br/>
+                        <input type='password' name='Wachtwoord' id='Wachtwoord' maxlength="50"/><br/>
 
-                        <label for='Studentnr' >Student_nr*:</label><br/>
-                        <input type='number' name='Studentnr' id='Studentnr' maxlength="6" /><br/>
+                        <label for='password1'>verificatiewachtwoord:</label><br/>
+                        <input type='password' name='Wachtwoord1' id='Wachtwoord1' maxlength="50"/><br/>
 
-                        <p>* Verplicht in te vullen.</p>
-                        <input id='submit'type='submit' name='Submit' value='Registreren' /><br/>
-                    </form>
+                        <label for='Studentnr'>Student_nr*:</label><br/>
+                        <input type='number' name='Studentnr' id='Studentnr' maxlength="6"/><br/>
+
+        <p>* Verplicht in te vullen.</p>
+        <input id='submit' type='submit' name='Submit' value='Registreren'/><br/>
+        </form>
     </div>
 
-    </div>
+</div>
 
 <?php
 //Registration script
-if(isset($_POST["Submit"])){
+if (isset($_POST["Submit"])) {
     //The post values have to be the same as the form <name> tag
     $firstname = $_POST['Voornaam'];
     $lastname = $_POST['Achternaam'];
     $email = $_POST['Email'];
     $password = $_POST['Wachtwoord'];
+    $password1 = $_POST['Wachtwoord1'];
     $studentnr = $_POST['Studentnr'];
 
-    if (empty ($_POST['Voornaam'])
-        || empty ($_POST['Achternaam'])
-        || empty ($_POST['Email'])
-        || empty ($_POST['Wachtwoord'])
-        || empty ($_POST['Studentnr']))
-    {
+    $firstname = str_replace(array('\'','"'),"",$firstname);
+
+
+    //Verifcation
+    if (empty ($_POST['Voornaam']) || empty ($_POST['Achternaam']) || empty ($_POST['Email']) || empty ($_POST['Wachtwoord']) || empty ($_POST['Wachtwoord1']) || empty ($_POST['Studentnr'])) {
         echo "please fill in all fields";
-    }
-    else
-        {
-        //hasing the password
-        $password = password_encrypt($_POST["Wachtwoord"]);
-        //defining the query
-        $sql  = "INSERT INTO user";
-        $sql .= "(Voornaam, Achternaam, Email, Wachtwoord, Studentnr, Verified, Type) ";
-        $sql .= "VALUES ('$firstname', '$lastname', '$email', '$password', '$studentnr', 1, 'student')";
+    } else {
 
-        $result = mysqli_query($connection, $sql);
-        header("Location: hidden.login.php");
+        /* Password Matching Validation */
+        if ($password != $password1) {
+            echo "Passwords moeten hetzelfde zijn<br>";
 
-        ob_end_flush();
+        } else {
 
-        if($result === false){
-            echo"ERROR".mysqli_errno()." : ".mysqli_error();
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "Enter a Valid email";
+            } else {
+
+                if (strlen($password) <= 6) {
+                    echo "Choose a password longer then 6 character";
+                    break;
+                } else {
+
+                    $queryUsers = "SELECT Email FROM user WHERE Email='$email'";
+                    $resultemail = mysqli_query($connection, $queryUsers);
+                    if (mysqli_num_rows($resultemail) > 0) {
+                        echo '<div class="">' . '* Email adres bestaat al' . "</div>";
+                    } else {
+
+                        $queryUsers = "SELECT Studentnr FROM user WHERE Studentnr='$studentnr'";
+                        $resultstnumber = mysqli_query($connection, $queryUsers);
+                        //$rowUsers = mysqli_fetch_array($resultUsers);
+                        if (mysqli_num_rows($resultstnumber) > 0) {
+                            echo '<div class="">' . '* Studentnumber bestaat al' . "</div>";
+                        } else {
+
+                            //hasing the password
+                            $password = password_encrypt($_POST["Wachtwoord"]);
+
+                            $sql = "INSERT INTO user";
+                            $sql .= "(Voornaam, Achternaam, Email, Wachtwoord, Studentnr, Verified, Type) ";
+                            $sql .= "VALUES ('$firstname', '$lastname', '$email', '$password', '$studentnr', 1, 'student')";
+
+                            $result = mysqli_query($connection, $sql);
+                            header("Location: hidden.login.php");
+
+                            ob_end_flush();
+
+                            if ($result === false) {
+                                echo "ERROR" . mysqli_errno() . " : " . mysqli_error();
+
+
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
 ?>
 
 </p>
-    </div>
+</div>
 </div>
 
 <div class="container-fluid bg-3 text-center">
 
-</div><br>
+</div>
+<br>
 
 <div class="container-fluid bg-3 text-center">
     <div class="row">
 
     </div>
-</div><br><br>
+</div>
+<br><br>
 
 
 <?php
