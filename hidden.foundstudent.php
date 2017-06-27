d5ed28306c261a164db0557f9ad59d42b84cb49f
Bakkertje01 <ernst-janbakker@live.nl>
2017-06-27 16:38:59 +0200

    foundstudent aangepast

--- hidden.foundstudent.php
+++ hidden.foundstudent.php
@@ -6,44 +6,39 @@ if($_SESSION["Type"] != "admin" && $_SESSION["Type"] != "slb" && $_SESSION["Type
     header("Location: Mijn%20Uploads.php");
 }
 
-function dateSelect($datum, $folder)
+function dateSelect($datum, $folder, $verified)
 {
+    $numberOfFiles = 0;
 
-	$dirnamez = "./studentuploads/" . $datum;
+    $dirnamez = "./studentuploads/" . $datum;
 
-	$files1 = glob("$dirnamez/" . "$folder/" . "*");
-
-
-
-	if (!empty($folder)) {
+    $files1 = glob("$dirnamez/" . "$folder/" . "*");
 
 
+    if (!empty($folder)) {
+        foreach (array_reverse($files1) as $file1) {
+            $numberOfFiles = count($file1);
+        }
 
-		foreach (array_reverse($files1) as $file1) {
-			$numberOfFiles = count($file1);
-		}
+        if ($numberOfFiles > 0) {
 
-		if (empty($numberOfFiles)){
-			$numberOfFiles = 0;
-		}
-
-		if ($numberOfFiles >= 1) {
-
-			foreach (array_reverse($files1) as $file1) {
-				$filetypes = explode('.', $file1);
-				$fileTitle = explode('--', $file1);
+            foreach (array_reverse($files1) as $file1) {
+                $filetypes = explode('.', $file1);
+                $fileTitle = explode('--', $file1);
 
 
-				if ($filetypes[2] == 'jpg' || $filetypes[2] == 'png' || $filetypes[2] == 'jpeg' || $filetypes[2] == 'JPG' || $filetypes[2] == 'gif') {
+                if ($filetypes[2] == 'jpg' || $filetypes[2] == 'png' || $filetypes[2] == 'jpeg' || $filetypes[2] == 'JPG' || $filetypes[2] == 'gif') {
 
-					$deftitle = ucfirst($fileTitle[1]);
+                    $deftitle = ucfirst($fileTitle[1]);
 
 
-					echo " <div class='col-sm-3'><h5>$deftitle</h5>";
+                    echo " <div class='col-sm-3'><h5>$deftitle</h5>";
 
-					echo "<a href='$file1' download><img src='$file1' title='$deftitle' class='img-responsive' style='width:100%' alt='$file1'></a>";
+                    echo "<a href='$file1' download><img src='$file1' title='$deftitle' class='img-responsive' style='width:100%' alt='$file1'></a>";
 
-					echo "</div>";
+                    echo $verified;
+
+                    echo "</div>";
 
 
 				}
@@ -63,12 +58,15 @@ function dateSelect($datum, $folder)
 
 					echo "<a href='$file1' target='_blank'><img src='thumbnails/$thumbnail' title='$deftitle' class='img-responsive' style=\"width:100%\" alt=\"$file1\"></a>";
 
-					echo "</div>";
+                    echo $verified;
+
+
+                    echo "</div>";
 				}
 
 			}
 		} else {
-			$numberOfFiles = NULL;
+			//$numberOfFiles = NULL;
 			echo "<p>Deze map bevat geen bestanden</p>";
 		}
 
@@ -107,6 +105,7 @@ include_once 'hidden.menu.php';
 if(isset($_GET['student'])) {
 	$studentnr = $_GET['student'];
 
+    $veriF = $row['Verified'];
 
 	$sql = "SELECT Gebruiker_ID, Voornaam, Studentnr, Quote, Achternaam, img_path, color_path FROM user WHERE Studentnr = '$studentnr'";
 	$result = $connection->query($sql);
@@ -182,31 +181,56 @@ color: $textColor;
 
 </style>";
 
-			$profielfoto = "studentuploads/$studentnumber/Profielfoto/pf.jpg";
-
-			$checkPf = "studentuploads/$studentnumber/Profielfoto/$PfNaam";
-
-			if (!file_exists($checkPf)) {
-				$profielfoto = "studentuploads/default/Profielfoto/$PfNaam";
-			}
-
-			echo "    <div class='container text-center'>
+    $profielfoto = "studentuploads/$studentnumber/Profielfoto/pf.jpg";
+    $checkPf = "studentuploads/$studentnumber/Profielfoto/$PfNaam";
+    if (!file_exists($checkPf)) {
+        $profielfoto = "studentuploads/default/Profielfoto/$PfNaam";
+    }
+    echo "    <div class='container text-center'>
         <h3>Bestanden van  $studentnaam  $studentachter2</h3>
-
         <img width='20%' src='$profielfoto' alt='profielfoto'
              title='Profielfoto'>
         <p><i>' $studentquote'</i></p>
     </div>";
 
 
+
+?>
+
+<div class="container text-center">
+
+<?php
+
 			$subdirs = array('CV', 'Afbeeldingen', 'Documenten');
 
+            switch ($indelingUser) {
+                case "indeling1":
+                    $subdirs = array('CV', 'Documenten', 'Afbeeldingen');
+                    break;
+                case "indeling2":
+                    $subdirs = array('CV', 'Afbeeldingen', 'Documenten');
+                    break;
+                case "indeling3":
+                    $subdirs = array('Documenten', 'CV', 'Afbeeldingen');
+                    break;
+                case "indeling4":
+                    $subdirs = array('Documenten', 'Afbeeldingen', 'CV');
+                    break;
+                case "indeling5":
+                    $subdirs = array('Afbeeldingen', 'Documenten', 'CV');
+                    break;
+                case "indeling6":
+                    $subdirs = array('Afbeeldingen', 'CV', 'Documenten');
+                    break;
+            }
 
-			foreach ($subdirs as $subdir) {
 
-				echo "<div class=\"container text-center\"><p>";
+
+foreach ($subdirs as $subdir) {
+
+				echo "<div class="container text-center"><p>";
 				echo "<h4>$subdir van $studentnaam</h4>";
-				echo dateSelect($studentnumber, $subdir);
+                echo dateSelect($studentnumber, $subdir, $veriF);
 				echo "</p><br><br></div>";
 			}
 
